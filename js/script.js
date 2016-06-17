fc81ae4dab353bb34ee180ae576ab97731bc5b7d
Kndarp Patel <kndarppatel@gmail.com>
2016-06-17 14:31:53 +0530

    Multi-Series Line Chart

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ plotThefts = function(){
               };
       });
       d.total = d.thefts[d.thefts.length - 1].y1;
-      console.log(d);
     });
 
     // Replace if sort required.
@@ -93,6 +92,131 @@ plotThefts = function(){
           .text(function(d) {return d;});
 
   });
-}
+};
+
+
+
+plotAssaults = function(){
+  var margin  = {
+    top     : 20,
+    right   : 80,
+    bottom  : 30,
+    left    : 50
+  },
+  width       = 960 - margin.left - margin.right,
+  height      = 500 - margin.top - margin.bottom,
+  parseYear   = d3.time.format("%Y").parse,
+  x           = d3.time.scale().range([0,width]),
+  y           = d3.scale.linear().range([height, 0]);
+  color       = d3.scale.category10();
+  xAxis       = d3.svg.axis().scale(x).orient("bottom"),
+  yAxis       = d3.svg.axis().scale(y).orient("left"),
+  line        = d3.svg.line()
+                  .interpolate("basis")
+                  .x(function(d){
+                      return x(d.year);
+                    })
+                  .y(function(d){
+                      return y(d.count);
+                    }),
+  svg         = d3.select(".multiline-series")
+                  .append("svg")
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                  .append("g")
+                  .attr("transform", "translate("+ margin.left + "," + margin.top +")");
+
+  d3.json("assault.json",function(err, data){
+
+    if(err) throw err;
+
+    color.domain(d3.keys(data[0]).filter(function(key){
+      return key !== "YEAR";
+    }));
+
+    data.forEach(function(d){
+      d.year = parseYear(d["YEAR"].toString());
+    });
+
+    var colors = color.domain().map(function(name){
+      return {
+        name: name,
+        values: data.map(function(d){
+          return {
+            year: d.year, count: +d[name]
+          };
+        })
+      };
+    });
+
+    x.domain(d3.extent(data, function(d){
+      return d.year;
+    }));
+
+    y.domain([
+      d3.min(colors, function(c){
+        return d3.min(c.values, function(v){
+          return v.count;
+        });
+      }),
+      d3.max(colors, function(c){
+        return d3.max(c.values, function(v){
+          return v.count;
+        });
+      })
+    ]);
+
+    svg.append("g")
+        .attr("class","x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y",6)
+        .attr("dy", ".5em")
+        .style("text-anchor", "end")
+        .text("Counts");
+
+    var count = svg.selectAll(".count")
+                    .data(colors)
+                    .enter().append("g")
+                    .attr("class", "count");
+
+    count.append("path")
+          .attr("class", "line")
+          .attr("d", function(d){
+            return line(d.values);
+          })
+          .style("stroke", function(d){
+            return color(d.name);
+          });
+
+    count.append("text")
+          .datum(function(d){
+            return {
+              name : d.name,
+              value: d.values[d.values.length - 1]
+            };
+          })
+          .attr("transform", function(d){
+            return "translate(" + x(d.value.year) + "," + y(d.value.count) + ")";
+          })
+          .attr("x", 1)
+          .attr("dy", ".25em")
+          .text(function(d){
+            return d.name;
+          });
+
+  });
+
+
+};
+
+
 
 plotThefts();
+plotAssaults();
